09680d6868e7e36ff4d3691d3be2e01f2566e040
Adam Abdel-Halim <adaab301@student.liu.se>
2019-04-08 19:37:02 +0200

    Finished program

--- Lab1/Lab1/Lab1/Lab1.asm
+++ Lab1/Lab1/Lab1/Lab1.asm
@@ -6,77 +6,58 @@
  */ 
 
 
-	.def	startBit = r20	; 1 bit siffra
-	.def	counter = r21
-	ldi		r18,$0A			; Delay T, 5 för T/2
-	ldi		r20,$01
-	clr		r21
-	ldi		r16,HIGH(RAMEND)
-	out		SPH,r16
-	ldi		r16,LOW(RAMEND)
-	out		SPL,r16
-	call	INIT
+.def counter = r21
+	clr	counter 
 
+	ldi r16,HIGH(RAMEND)
+	out SPH,r16
+	ldi	r16,LOW(RAMEND)
+	out SPL,r16
+	rcall INIT
+	clr	  r19
 
-	FOREVER:
-		clr		startBit
-		sbis	PINA,0
-		call	FOREVER
-		ldi		r18,5		; Delay = T/2
-		call	DELAY
-		sbis	PINA,0
-		call	FOREVER
-		dec		startBit
-		clr		r19			; Clearar output
-		
-	LOOP:
-		ldi		r18,10		; Delay = T
-		ldi		r23,$01
-		ldi		r22,$03
-		cpi		startBit,0	
-		breq	FOREVER
-		call	DELAY
-		ldi		r16,PINA	
-		andi	r16,$01		; Maskar ut sista biten
-		cpse	r16,r23
-		lsr		r19
-		call	insertBit	
-		cpi		counter,$03
-		breq	displayOutput
-		inc		counter
-		call	LOOP
-		
-		
-	displayOutput:
-		out		PORTB,r19
-		clr		counter
-		call	FOREVER
+STARTBIT:
+	sbis PINA,0
+	rjmp STARTBIT
+	ldi r18,7		;Delay T/2
+	rcall DELAY
+	sbis PINA,0
+	rjmp STARTBIT
+	ldi r18,2*7		;Delay
+DATABIT:
+	lsr r19
+	clr r22
+	rcall DELAY
+	sbic PINA,0
+	ldi	r22,$08
+	add r19,r22
+	inc counter	; Z
+	cpi counter,$04
+	breq DATABIT
+	;rjmp DATABIT
+DISPLAY:
+	out PORTB,r19
+	clr r19
+	clr counter
+	;rcall DELAY
+	rjmp STARTBIT
 
-	insertBit:
-		ldi		r16,$08
-		add		r19,r16
-		cpse	counter,r22	
-		lsr		r19			; Skifta 1 steg höger
-		ret
-		
-		
+INIT:
+	clr r16
+	out DDRA,r16
+	ldi r16,$0F
+	out DDRB,r16
+	ret
 
-	INIT:
-		clr	r16
-		out DDRA,r16
-		ldi	r16,$0F
-		out	DDRB,r16
-		ret
-
-	DELAY:
-		sbi	PORTB,7
-		mov	r16,r18		; Decimal bas
-	delayYttreLoop:
-		ldi	r17,$1F
-	delayInreLoop:
-		dec	r17
-		brne	delayInreLoop
-		dec	r16
-		brne	delayYttreLoop
-		cbi	PORTB,7
-		ret
+DELAY:
+	sbi PORTB,7
+	mov r16,r18
+delayYttreLoop:
+	ldi r17,$1F
+delayInreLoop:
+	dec r17
+	brne delayInreLoop
+	dec r16
+	brne delayYttreLoop
+	cbi PORTB,7
+	ret
