0e4fe72ba9974ff9531cd10aa9d7f922dc811c31
Adam Abdel-Halim <adaab301@student.liu.se>
2019-05-02 15:33:58 +0200

    Added space support (First char bug, beep to short)

--- Lab2/Lab2/Lab2.asm
+++ Lab2/Lab2/Lab2.asm
@@ -6,6 +6,7 @@ ldi		N_length,$55
  
 ldi     r16,HIGH(RAMEND)
 out     SPH,r16
+
 ldi     r16,LOW(RAMEND)
 out     SPL,r16
 ldi     r16,$01
@@ -14,26 +15,27 @@ out     DDRB,r16
  
  
 MORSE:
-            ldi ZL,LOW(MESSAGE*2)
-            ldi ZH,HIGH(MESSAGE*2)
-            rcall GET_CHAR
-			LOOP:
-            breq KLAR
-            rcall LOOKUP
-            rcall SEND
-            ldi beep_length,$03
-            rcall NO_BEEP
-			rcall GET_CHAR
-			cpi char,$00
-			brne LOOP
-            rjmp MORSE
+        ldi		ZL,LOW(MESSAGE*2)
+        ldi		ZH,HIGH(MESSAGE*2)
+        rcall	GET_CHAR
+	LOOP:
+        breq	KLAR
+        rcall	LOOKUP
+        rcall	SEND
+        ldi		beep_length,$03
+        rcall	NO_BEEP
+		rcall	GET_CHAR
+		cpi		char,$00
+		brne	LOOP
+        rjmp	MORSE
 LOOKUP:
             push ZL
             push ZH
             ldi ZH,HIGH(BTAB*2)
             ldi ZL,LOW(BTAB*2)
-            subi char,$41 ;ändra till 40 med space sedan.
-			ldi r22, 1 
+            subi char,$40
+			brmi	SPACE ;ändra till 40 med space sedan.
+CONTINUE:	ldi r22, 1 
             add ZL,char
 			brcc pc+2
 			add	ZH, r22
@@ -41,18 +43,22 @@ LOOKUP:
             pop ZH
             pop ZL
             ret
-GET_CHAR:
-            lpm char,Z+
-            ret
-SEND:
+SPACE:		ldi beep_length,$07
+			ldi	char,$00
+			rjmp	CONTINUE
+
+SEND:		
             rcall GET_BIT
             cpi char_bin,$00
-            breq CHAR_DONE
+            breq CHAR_DONE	
             rcall BEEP
             ldi beep_length,$01
             rcall NO_BEEP
             rjmp SEND
 char_done:  ret
+GET_CHAR:
+            lpm char,Z+
+            ret
 GET_BIT:
             ldi beep_length,$01
             sbrc char_bin,7
@@ -84,17 +90,11 @@ CYCLE1:
             cbi PORTB,0
             rcall DELAY
             ret
-CYCLE2:    
-            cbi PORTB,0
-            rcall DELAY
-            cbi PORTB,0
-            rcall DELAY
-            ret
 KLAR:
 DELAY:
-            ldi r16,$01
+            ldi r16,$02
 delayYttreLoop:
-            ldi r17,$AA
+            ldi r17,$FF
 delayInreLoop:
             dec r17
             brne delayInreLoop
@@ -103,7 +103,7 @@ delayInreLoop:
             ret
            
 .org $200
-MESSAGE:    .db "DATORTEKNIK",$00
+MESSAGE:    .db "AA BB CC",$00
  
 .org $230
-BTAB:       .db $60, $88, $A8, $90, $40, $28, $D0, $08, $20, $78, $B0, $48, $E0, $A0, $F0, $68, $D8, $50, $10, $C0, $30, $18, $70, $98, $B8, $C8
+BTAB:       .db $00, $60, $88, $A8, $90, $40, $28, $D0, $08, $20, $78, $B0, $48, $E0, $A0, $F0, $68, $D8, $50, $10, $C0, $30, $18, $70, $98, $B8, $C8
